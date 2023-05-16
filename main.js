eb3b81fcb707846c2e8b2e940ab4a3b901fbf436
kvanitp2 <evgss722@gmail.com>
2023-05-16 11:51:28 +0500

    init

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const links = [...document.querySelectorAll('.nav .nav-link')]
-
+let autoPlay = true
 function removeActive() {
     links.forEach(link => link.classList.remove('active'))
 }
@@ -26,7 +26,28 @@ slides.forEach((el, i) => {
     newCircle.setAttribute('number', i)
     i === 0 && newCircle.classList.add('active')
     newCircle.addEventListener('click', (e) => {
-        console.log(e.target)
+        const index = parseInt(e.target.getAttribute('number'))
+        reset(index)
+        autoPlay = false
     })
     navigation.append(newCircle)
 })
+
+function reset(num) {
+    slides.forEach(slide => slide.classList.remove('active'))
+    const icons = document.querySelectorAll('.circle')
+    icons.forEach(icon => icon.classList.remove('active'))
+    slides[num].classList.add('active')
+    icons[num].classList.add('active')
+}
+
+setInterval(() => {
+    if (autoPlay) {
+        const currentIndex = document.querySelector('.circle.active')
+        let index = parseInt(currentIndex.getAttribute('number')) + 1
+        if (index === slides.length) {
+            index = 0
+        }
+        reset(index)
+    }
+}, 1000)
